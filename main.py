db409fbe741058276f21bc6d87e77925492c4494
Montassir <100169908+Montassi@users.noreply.github.com>
2023-09-14 12:43:28 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from model import *
+
+#query = "ما هي عقوبات تجاوز السرعة المسموح بها؟"
+query = "What are the speed limits?"
+result = final_result(query)
+'''
+ما هي عقوبات تجاوز السرعة المسموح بها؟
+
+'''
